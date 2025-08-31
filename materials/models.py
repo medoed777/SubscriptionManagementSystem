a249df6f54470cb8ef0e37ba91d0d6d8cd8110a7
Alexey <mister.skorpion4ik@mail.ru>
2025-08-31 15:27:33 +0500

    realise models and migrate

--- materials/models.py
+++ materials/models.py
@@ -1,9 +1,51 @@
 from django.db import models
 
 
-class Lesson(models.Model):
-    pass
-
-
 class Course(models.Model):
-    pass
+
+    name = models.CharField(
+        max_length=100, verbose_name="Название", help_text="Введите название курса"
+    )
+
+    description = models.TextField(
+        verbose_name="Описание", help_text="Введите описание курса"
+    )
+
+    preview = models.ImageField(
+        upload_to="preview/", blank="True", null="True", verbose_name="Превью", help_text="Установите картинку"
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Курс"
+        verbose_name_plural = "Курсы"
+
+
+class Lesson(models.Model):
+
+    name = models.CharField(
+        max_length=100, verbose_name="Название", help_text="Введите название урока"
+    )
+
+    description = models.TextField(
+        verbose_name="Описание", help_text="Введите описание урока"
+    )
+
+    preview = models.ImageField(
+        upload_to="preview/", blank="True", null="True", verbose_name="Превью"
+    )
+
+    link = models.CharField(
+        max_length=150, blank="True", null="True", verbose_name="Ссылка", help_text="Введите ссылку на урок"
+    )
+
+    course = models.ForeignKey(Course, on_delete=models.SET_NULL, null=True, verbose_name="Курс", help_text="Выберите курс")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Урок"
+        verbose_name_plural = "Уроки"
--- users/models.py
+++ users/models.py
@@ -1,5 +1,24 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
-class User(models.Model):
-    pass
+class User(AbstractUser):
+    username = None
+
+    email = models.EmailField(
+        unique=True, verbose_name="Email", help_text="Введите почту"
+    )
+    avatar = models.ImageField(
+        upload_to="avatar/", blank=True, null=True, verbose_name="Аватар", help_text="Установите аватар"
+    )
+    phone_number = models.CharField(
+        max_length=30, blank=True, null=True, verbose_name="Номер телефона", help_text="Укажите номер телефона"
+    )
+    city = models.CharField(max_length=50, blank=True, null=True, verbose_name="Город", help_text="Укажите город")
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
