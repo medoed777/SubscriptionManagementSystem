c32183d7a17f5a719ab18d0b7e88ae6fe2b41b21
Alexey <mister.skorpion4ik@mail.ru>
2025-09-02 13:03:00 +0500

    fixed setting

--- config/settings.py
+++ config/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "users",
     "materials",
+    "django-filter",
 ]
 
 MIDDLEWARE = [
@@ -137,3 +138,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "users.User"
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': (
+        'django_filters.rest_framework.DjangoFilterBackend',
+    ),
+
+}
