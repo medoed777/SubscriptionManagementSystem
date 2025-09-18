0c1c24d8b8ba1abd2aadda90a28715b393c3e2b7
Alexey <mister.skorpion4ik@mail.ru>
2025-09-18 12:42:26 +0500

    add eventlet

--- config/celery.py
+++ config/celery.py
@@ -2,7 +2,10 @@ from __future__ import absolute_import, unicode_literals
 
 import os
 
+import eventlet
 from celery import Celery
+
+eventlet.monkey_patch()
 
 # Установка переменной окружения для настроек проекта
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
